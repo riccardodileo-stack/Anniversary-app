09aee8063646bf678ee7f5eb67dab214a6483e3b
riccardodileo-stack <riccardo.dileo@icloud.com>
2026-07-16 16:07:35 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const APP_CACHE = "our-journey-shell-v1";
-const RUNTIME_CACHE = "our-journey-runtime-v1";
-const MAP_CACHE = "our-journey-map-v1";
+const APP_CACHE = "our-journey-shell-v2";
+const RUNTIME_CACHE = "our-journey-runtime-v2";
+const MAP_CACHE = "our-journey-map-v2";
 
 const APP_SHELL = [
   "./",
